a77d25efccbfebd4a5947cff6e37e5e2a4a2a460
Nils Backlund <nils.backlund@vindiseglen.com>
2025-06-16 17:11:01 +0200

    base for main loop added

---
+++ config.py
@@ -0,0 +1,14 @@
+# %%
+from Red.model import LLMModel
+
+llm_model = LLMModel.GPT_4O_MINI
+simulate_command_line = True
+save_logs = True
+save_configuration = True
+
+attacks_per_configuration = 10
+
+# %%
+if simulate_command_line:
+    save_logs = False
+    

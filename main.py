a9897fa2d85c6ac7e16ea29388bd607b1c57fbc1
LeonieGr <lonie02@yahoo.com>
2025-06-17 11:06:48 +0200

    Add get base config

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import uuid
 from config import attacks_per_configuration, save_logs, n_configurations
 from Red.sangria import run_attacks, save_logs_to_file
-from Blue.new_config_pipeline import generate_new_honeypot_config
+from Blue.new_config_pipeline import generate_new_honeypot_config, save_config_as_file
 
 def main():
     base_honeypot_config = get_base_config(id=None)
@@ -10,20 +10,19 @@ def main():
 
     for i in range(n_configurations):
         print(f"Iteration {i + 1} / 10")
-        session_id = uuid.uuid4()
         
         if i != 0:
-            new_config = generate_new_honeypot_config()
+            config_id, new_config = generate_new_honeypot_config()
             set_honeypot_config(new_config)
 
         full_logs = run_attacks(n_attacks=attacks_per_configuration, save_logs=save_logs)
         if not full_logs:
             continue
-        save_logs_to_file(full_logs, session_id, save_logs)
+        save_logs_to_file(full_logs, config_id, save_logs)
 
         format_logs_for_network()
 
-        save_configuration(new_config)
+        save_config_as_file(new_config)
         save_labels()
 
 

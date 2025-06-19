ea9d0537ad8e86a6979f32240be0cf91b54eb763
danielstoopedahl <daniel.stoopedahl@gmail.com>
2025-06-19 10:06:38 +0200

    fixed dns names of honeypot

--- Blue_Lagoon/honeypot_tools.py
+++ Blue_Lagoon/honeypot_tools.py
@@ -3,8 +3,8 @@ import subprocess
 def start_dockers():
     try:
         print("Starting Docker containers...")
-        subprocess.run(["sudo", "docker-compose","-f", "Honeypot/docker-compose.yml", "build"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        subprocess.run(["sudo", "docker-compose", "-f", "Honeypot/docker-compose.yml","up", "-d"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.run(["sudo", "docker-compose","-f", "Blue_Lagoon/docker-compose.yml", "build"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.run(["sudo", "docker-compose", "-f", "Blue_Lagoon/docker-compose.yml","up", "-d"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
         print("Docker containers started")
     except subprocess.CalledProcessError as e:
@@ -13,7 +13,7 @@ def start_dockers():
 def stop_dockers():
     try:
         print("Stopping Docker containers...")
-        subprocess.run(["sudo", "docker-compose", "-f", "Honeypot/docker-compose.yml","down"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.run(["sudo", "docker-compose", "-f", "Blue_Lagoon/docker-compose.yml","down"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
         print("Docker containers stopped")
     except subprocess.CalledProcessError as e:

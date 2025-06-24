18a9b1fc60a62ff4a3817289634e69de28902ab7
danielstoopendahl <daniel.stoopendahl@gmail.com>
2025-06-24 13:33:49 +0200

    refactored config

--- config.py
+++ config.py
@@ -3,12 +3,14 @@ from Red.model import LLMModel
 
 llm_model_sangria = LLMModel.GPT_4O_MINI
 llm_model_config = LLMModel.GPT_4O_MINI
-simulate_command_line = True
+simulate_command_line = False
 save_logs = True
 save_configuration = True
 print_output = True
-attacks_per_configuration = 25
 n_configurations = 1
+attacks_per_configuration = 25
+max_session_length = 50
+
 honeypot = "beelzebub" # beelzebub / cowrie
 
 # %%

177facd2525fbde7635324b056d3ecf402e3ce1c
danielstoopendahl <daniel.stoopendahl@gmail.com>
2025-07-09 16:51:55 -0500

    fixed docker bug

--- Blue_Lagoon/honeypot_tools.py
+++ Blue_Lagoon/honeypot_tools.py
@@ -2,8 +2,8 @@ import subprocess
 from config import honeypot
 
 def init_docker():
-    subprocess.run(["sudo", "docker", "stop", "kali3"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    subprocess.run(["sudo", "docker", "rm", "kali3"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(["sudo", "docker", "stop", "kali4"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(["sudo", "docker", "rm", "kali4"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     subprocess.run(["sudo", "docker", "stop", "cow"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     subprocess.run(["sudo", "docker", "rm", "cow"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     subprocess.run(["sudo", "docker", "stop", "blue_lagoon"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)

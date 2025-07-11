e926405c43295cc01582e89ef1112b3eb14aefff
Sackarias Lunman <lunman@student.chalmers.se>
2025-07-11 15:06:05 -0500

    some rudimentary data analysis

--- Purple/Data_analysis/analysis.py
+++ Purple/Data_analysis/analysis.py
@@ -21,7 +21,7 @@ display(dropdown)
 #%%
 
 selected_experiment = dropdown.value
-filter_empty_sessions = True
+filter_empty_sessions = False
 print(f"Analyzing experiment {selected_experiment}")
 
 from Utils.jsun import load_json
@@ -33,6 +33,7 @@ path = logs_path / selected_experiment
 configs = [name for name in os.listdir(path) if str(name).startswith("hp_config")]
 
 sessions_list = [load_json(path / config / "sessions.json") for config in configs]
+
 if filter_empty_sessions:
     new_sessions_list = []
     for config_sessions in sessions_list:
@@ -42,12 +43,79 @@ if filter_empty_sessions:
 reconfig_indices = np.cumsum([len(session) for session in sessions_list][:-1])
 combined_sessions = sum(sessions_list, [])
 print(f"Reconfig indices: {reconfig_indices}")
+print(f"Number of sessions: {len(combined_sessions)}")
 
-# pprint.pprint(combined_sessions)
-# pprint.pprint(len(combined_sessions))
-# pprint.pprint(measure_session_length(combined_sessions))
-# pprint.pprint(measure_tactic_distribution(combined_sessions))
-# pprint.pprint(measure_unique_techniques(combined_sessions))
 
-#%%
+length_data = measure_session_length(combined_sessions)
+tactic_dist_data = measure_tactic_distribution(combined_sessions)
+unique_techniques_data = measure_unique_techniques(combined_sessions)
 
+
+#%% Plotting cumulative attack of unique techniqes vs sessions
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(12, 6))
+plt.plot(unique_techniques_data["session_cum_num_techniques"], marker="o", linestyle="--")
+plt.title("Cumulative sum of unique techniques")
+plt.xlabel("Session")
+plt.ylabel("Number of Unique Tactics")
+plt.ylim(bottom=0)
+plt.grid()
+plt.legend()
+plt.show()
+
+# %%
+
+plt.figure(figsize=(12, 6))
+plt.plot(unique_techniques_data["session_num_techniques"], marker="o", linestyle="--")
+plt.title("Number of Unique Tactics per Session")
+plt.xlabel("Session")
+plt.ylabel("Number of Unique Tactics")
+plt.grid()
+plt.ylim(bottom=0)
+plt.legend()
+plt.show()
+
+# %% Session length
+
+plt.figure(figsize=(12, 6))
+plt.plot(length_data["session_lengths"], marker="o", linestyle="--")
+plt.title("Session length per Session")
+plt.xlabel("Session")
+plt.ylabel("Session length")
+
+for index in reconfig_indices:
+    plt.axvline(index - 0.5, color="k", linestyle="--")
+
+plt.grid()
+plt.ylim(bottom=0)
+plt.legend()
+plt.show()
+
+# %% Tokens vs session
+
+tokens_list = [load_json(path / config / "tokens_used.json") for config in configs]
+token_reconfig_indices= np.cumsum([len(tokens) for tokens in tokens_list][:-1])
+combined_tokens = sum(tokens_list, [])
+
+prompt_tokens = [tokens["prompt_tokens"] for tokens in combined_tokens]
+cached_tokens = [tokens["cached_tokens"] for tokens in combined_tokens]
+completion_tokens = [tokens["completion_tokens"] for tokens in combined_tokens]
+
+plt.figure(figsize=(12, 6))
+plt.plot(prompt_tokens, marker="o", linestyle="--")
+# plt.plot(cached_tokens, marker="o", linestyle="--")
+plt.plot(completion_tokens, marker="o", linestyle="--")
+plt.title("Session length per Session")
+plt.xlabel("Session")
+plt.ylabel("Session length")
+
+for index in token_reconfig_indices:
+    plt.axvline(index - 0.5, color="k", linestyle="--")
+
+plt.grid()
+plt.ylim(bottom=0)
+plt.legend()
+plt.show()
+
+# %%

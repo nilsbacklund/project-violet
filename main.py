4799031f4f2555d8710df005cf70f3815559d2d7
LeonieGr <lonie02@yahoo.com>
2025-07-11 13:58:06 -0500

    Fix attcker+config info for reconfiguration

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ def main():
             if not config.simulate_command_line:
                 stop_dockers()
 
-            config_id, honeypot_config = generate_new_honeypot_config()
+            config_id, honeypot_config = generate_new_honeypot_config(base_path)
             set_honeypot_config(honeypot_config)
 
             config_counter += 1

0f55282bd7b20ed9eb7f5a1bdeadb489ecec96bb
LeonieGr <lonie02@yahoo.com>
2025-07-14 15:31:28 +0000

    changed colors for better contrast

--- Style/colors.py
+++ Style/colors.py
@@ -1,12 +1,12 @@
 # This is a modification of the
 #  IBM color palette from
-#  https://siegal.bio.nyu.edu/color-palette/ .
+#  https://davidmathlogic.com/colorblind/#%23517AE2-%23753CE2-%23DC263B-%23FE6100-%23FFB000 .
 #  Should be color blind friendly!
 
 black = "#000000"
-blue = "#648FFF"
+blue = "#517AE2"
 red = "#DC263B"
-purple = "#785EF0"
+purple = "#753CE2"
 yellow = "#FFB000"
 orange = "#FE6100"
 

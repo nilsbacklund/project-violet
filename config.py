2150993bef5cb05d33ce6aaacab283c5c84d561f
Nils Backlund <nils.backlund@vindiseglen.com>
2025-07-15 16:05:56 -0500

    updated config for more structure

--- config.py
+++ config.py
@@ -4,21 +4,34 @@ from Red.attacker_prompts import AttackerPrompts
 
 experiment_name = ""
 
+# Experiment settings
 llm_model_sangria = LLMModel.O4_MINI
 llm_model_config = LLMModel.GPT_4_1_MINI
 attacker_prompt: str = AttackerPrompts.GENERAL
-reconfig_method: str = ReconfigMethod.NEW_TECHNIQUES # Not implemented yet
+reconfig_method: str = ReconfigMethod.NEW_TECHNIQUES
 
+# Reconfiguration settings
+n_attacks = 20 # only relevant for EVERY_N_ATTACKS
+n_config_window_size = 5 # only relevant for NEW_TECHNIQUES
+saturation_limit = 0.5 # only relevant for NEW_TECHNIQUES
+
+# Ending configs for NO_RECONFIG
+end_window_size = 20
+end_saturation_limit = 0.06
+
+# General settings
 simulate_command_line = False
 save_logs = True
 save_configuration = True
 print_output = True
+
+# Session settings
 num_of_attacks = 100
 min_num_of_attacks_reconfig = 2
 max_session_length = 100
 
+# Other
 honeypot = "beelzebub" # beelzebub / cowrie
-
 ISO_FORMAT = "%Y-%m-%dT%H_%M_%S"
 
 # %%

7709c738609926b9431e5616c8dc1f80231de970
Sackarias Lunman <lunman@student.chalmers.se>
2025-07-17 09:59:36 -0500

    fixed bug in analysis and extraction

--- Purple/Data_analysis/metrics/unique_techniques.py
+++ Purple/Data_analysis/metrics/unique_techniques.py
@@ -5,15 +5,18 @@ def measure_unique_techniques(sessions: List[Dict]) -> Dict[str, Any]:
     config_techniques = set()
     session_techniques: List[Set] = []
     session_num_techniques: List[int] = []
-
+    session_cum_num_techniques: List[int] = []
     for session in sessions:
         current_techniques = str(session["techniques"]).split("--")
         current_techniques = {technique.split("-")[0].strip() for technique in current_techniques if technique != ""}
         config_techniques.update(current_techniques)
+        
         session_techniques.append(current_techniques)
         session_num_techniques.append(len(current_techniques))
+        session_cum_num_techniques.append(len(config_techniques))
 
-    session_cum_num_techniques = [int(num) for num in np.cumsum(session_num_techniques)]
+    print("config techniques:", config_techniques)
+    print("config techniques:", len(config_techniques))
 
     results = {
         "config_num_techniques": len(config_techniques),
